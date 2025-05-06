0207e67f8749ff12f3f73ef253b7bfcdbbb94945
Marco <111095859+Specktic@users.noreply.github.com>
2025-05-05 22:32:55 -0600

    Definir modulo SPI arduino

--- Lab3/top.sv
+++ Lab3/top.sv
@@ -117,6 +117,17 @@ module top (
         .hay_ganador(hay_ganador),
         .jugador_ganador(jugador_ganador)
     );
+	 
+	 // Arduino
+	 spi_slave_arduino spi_slave_inst (
+    .clk(clk),
+    .reset(reset),
+    .sck(SCK_ARDUINO),
+    .ss(SS_ARDUINO),
+    .mosi(MOSI_ARDUINO),
+    .col_arduino(COL_ARDUINO),
+    .arduino_valida_jugada(ARDUINO_VALIDA_JUGADA)
+)
 
     // Controlador VGA
     vga_controller vga (
@@ -132,5 +143,3 @@ module top (
 
 endmodule
 
-module two ();
-endmodule
